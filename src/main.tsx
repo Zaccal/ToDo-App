7713a90f95f6418008e7287e88c7e8c997bf3205
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-07-16 18:30:22 +0600

    init

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  </StrictMode>,
+)
