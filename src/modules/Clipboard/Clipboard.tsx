245b3f960bb57c5be8c650c896deb6ba8f2e59dc
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-08-03 23:56:47 +0600

    added Input for add new task

--- src/modules/Clipboard/Clipboard.tsx
+++ src/modules/Clipboard/Clipboard.tsx
@@ -1,9 +1,17 @@
 import ClipboardListTitle from "../../components/ClipboardListTitle/ClipboardListTitle"
+import { Input } from "../Sidebar/SidebarImports"
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 
 const Clipboard = () => {
   return (
     <div>
-      <ClipboardListTitle />
+      <ClipboardListTitle className="mb-11"/>
+      <Input 
+        icon={<AddRoundedIcon />} 
+        placeholder="Add New Task" 
+        classNameContainer="px-3 h-12"
+      />
+      
     </div>
   )
 }
