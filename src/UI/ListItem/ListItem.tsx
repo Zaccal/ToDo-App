90b0195c274dd112c0d2d5c8a533c7b962c6ae85
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-08-03 21:25:38 +0600

    change defualt color text

--- src/UI/ListItem/ListItem.tsx
+++ src/UI/ListItem/ListItem.tsx
@@ -11,7 +11,7 @@ interface IListItem {
 
 const ListItem = ({primary, icon, messageCount, active, onClick, className}: IListItem) => {
   return (
-    <div onClick={onClick} className={`${className || ''} ${active ? 'dark:bg-gray-400 bg-mute dark:text-slate-900 text-black' : 'text-main dark:text-gray-400'} ListItem`}>
+    <div onClick={onClick} className={`${className || ''} ${active ? 'dark:bg-gray-400 bg-mute dark:text-slate-900 text-[#212529]' : 'text-main dark:text-gray-400'} ListItem`}>
         <div className="flex items-center justify-start">
             {icon && (
                 <div className=" mr-2">
