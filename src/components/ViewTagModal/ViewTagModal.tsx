067818ccdfe6d09efcf60008091bbbe038e4b83e
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-08-15 23:54:26 +0600

    change ui viewTagModal text color

--- src/components/ViewTagModal/ViewTagModal.tsx
+++ src/components/ViewTagModal/ViewTagModal.tsx
@@ -1,6 +1,7 @@
 import { useGlobalStateContext } from "../../Providers/GlobalStateProvider/GlobalStateProvider"
 import List from "../../UI/List/List"
 import Modal from "../../UI/Modal/Modal"
+import { darkenColor } from "../../utils/utils"
 
 const ViewTagModal = () => {
   const {visibleTagModal, setVisibleTagModal, displayingTagModal} = useGlobalStateContext()
@@ -10,7 +11,9 @@ const ViewTagModal = () => {
       <div className="px-4 py-3 rounded-lg" style={{
         backgroundColor: displayingTagModal.color,
       }}>
-        <h2 className="text-xl text-slate-900"># {displayingTagModal.name}</h2>
+        <h2 style={{
+          color: darkenColor(displayingTagModal.color, 120)
+        }} className="text-xl"># {displayingTagModal.name}</h2>
       </div>
       <List titleSize={12} title="tasks:" className="mt-3 rounded-lg h-[428px]">
 
