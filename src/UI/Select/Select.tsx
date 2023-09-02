9f4a7948883466828eccf84e93a5bdd4a1a9b977
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-09-02 16:39:13 +0600

    Added Select ui component

---
+++ src/UI/Select/Select.tsx
@@ -0,0 +1,54 @@
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import { MouseEvent, useState } from 'react';
+
+export type TypeSelectOption = Record<'value' | 'label', string>
+
+interface ISelect {
+    options: TypeSelectOption[],
+    value?: TypeSelectOption,
+    onChange?: (value: TypeSelectOption) => void,
+    className?: string,
+}
+
+const Select = ({options, onChange, value, className}: ISelect) => {    
+    const [isOpen, setIsOpen] = useState(false)
+    const [isSelected, setIsSelected] = useState<TypeSelectOption | undefined>(value)
+
+    const selectHandler = (event: MouseEvent<HTMLLIElement>, newValue: TypeSelectOption) => {
+        event.stopPropagation()
+        setIsOpen(false)
+        setIsSelected(newValue)
+        if (onChange)
+            onChange(newValue)
+    }
+
+    
+    return (
+        <div 
+            onBlur={() => setIsOpen(false)}
+            onClick={() => setIsOpen(prev => !prev)} 
+            tabIndex={0} 
+            className={`${className || ''} Select`}
+        >
+            <span className="flex-grow">{value?.label}</span>
+            <button className="cursor-pointer text-lg focus:text-main hover:text-main">
+                <KeyboardArrowDownRoundedIcon />
+            </button>
+            <ul className={`Options ${isOpen ? 'show' : ''}`}>
+                {
+                    options.map(optionData => (
+                        <li 
+                            key={optionData.value} 
+                            onClick={event => selectHandler(event, optionData)} 
+                            className={`Option ${isSelected?.value === optionData.value ? 'selected' : ''}`}
+                        >
+                            {optionData.label}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+  )
+}
+
+export default Select
