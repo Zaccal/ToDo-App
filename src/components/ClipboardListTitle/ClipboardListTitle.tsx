d7230c421b79ca043d3193d74754f61b6d511ee6
Zhaltyrbaev Adil <ksss90411@gmail.com>
2023-10-28 18:02:08 +0600

    added function to delete list

--- src/components/ClipboardListTitle/ClipboardListTitle.tsx
+++ src/components/ClipboardListTitle/ClipboardListTitle.tsx
@@ -1,23 +1,27 @@
-import { useState, KeyboardEvent, useEffect } from "react";
-import MessageCountTitle from "../../UI/MessageCountTitle/MessageCountTitle";
-import useGetNowActiveList from "../../hooks/useGetNowActiveList";
-import Input from "../../UI/Input/Input";
-import { useLocalStorageContext } from "../../Providers/LocalStorageProvider/LocalStorageProvider";
-import { getListTaskDoneCount } from "../../utils/utils";
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { useGlobalStateContext } from "../../Providers/GlobalStateProvider/GlobalStateProvider";
+import { useState, KeyboardEvent, useEffect } from "react"
+import MessageCountTitle from "../../UI/MessageCountTitle/MessageCountTitle"
+import useGetNowActiveList from "../../hooks/useGetNowActiveList"
+import Input from "../../UI/Input/Input"
+import { useLocalStorageContext } from "../../Providers/LocalStorageProvider/LocalStorageProvider"
+import { getListTaskDoneCount } from "../../utils/utils"
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded"
+import { useGlobalStateContext } from "../../Providers/GlobalStateProvider/GlobalStateProvider"
+import Button from "../../UI/Button/Button"
+import DeleteRounded from "@mui/icons-material/DeleteRounded"
+import Alert from "../Alert/Alert"
 
 interface IClipboardListTitle {
     className?: string
 }
 
-const ClipboardListTitle = ({className}: IClipboardListTitle) => {
+const ClipboardListTitle = ({ className }: IClipboardListTitle) => {
     const nowActiveList = useGetNowActiveList()
-    const {listsStore, setListsStore, settingsStore} = useLocalStorageContext()
+    const { listsStore, setListsStore, settingsStore } = useLocalStorageContext()
     const [editMode, setEditMode] = useState(false)
     const [listName, setListName] = useState(nowActiveList.name)
     const isScreenMd = window.innerWidth <= 1024 ? true : false
-    const {setIsOpenSidebarMenu} = useGlobalStateContext()
+    const { setIsOpenSidebarMenu } = useGlobalStateContext()
+    const [alertConfirmationToDeleteList, setAlertConfirmationToDeleteList] = useState(false)
 
     useEffect(() => {
         setListName(nowActiveList.name)
@@ -30,56 +34,98 @@ const ClipboardListTitle = ({className}: IClipboardListTitle) => {
     }
 
     const changeNameHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter' && event.currentTarget.value.length >= 1) {            
+        if (event.key === "Enter" && event.currentTarget.value.length >= 1) {
             setEditMode(false)
             setListName(event.currentTarget.value)
 
-            setListsStore(listsStore.map(listData => {
-                if (listData.id === nowActiveList.id) {
-                    return {...listData, name: listName}
-                }
+            setListsStore(
+                listsStore.map(listData => {
+                    if (listData.id === nowActiveList.id) {
+                        return { ...listData, name: listName }
+                    }
 
-                return listData
-            }))
+                    return listData
+                })
+            )
         }
     }
 
+    const deleteListHandler = () => {
+        setListsStore(listsStore.filter(listData => listData.id !== nowActiveList.id))
+        setListsStore(prev =>
+            prev.map((listData, index) => {
+                if (index === prev.length - 1) {
+                    return {
+                        ...listData,
+                        active: true,
+                    }
+                }
+
+                return listData
+            })
+        )
+    }
+
     return (
-        <div onClick={switchEditModeHandler} className={`${className || ''} flex items-center justify-between`}>
-            <div className="flex items-center">
+        <div className={`${className || ""} flex items-center justify-between`}>
+            <div onClick={switchEditModeHandler} className="flex items-center">
                 {!editMode && (
-                    <h1 style={{
-                        cursor: nowActiveList.accessEdit ? 'pointer' : 'auto', 
-                    }} className="text-5xl font-black dark:text-white">
+                    <h1
+                        style={{
+                            cursor: nowActiveList.accessEdit ? "pointer" : "auto",
+                        }}
+                        className="text-5xl font-black dark:text-white"
+                    >
                         {nowActiveList.name}
                     </h1>
                 )}
                 {editMode && (
-                    <Input 
-                        autoFocus 
+                    <Input
+                        autoFocus
                         value={listName}
-                        onKeyUp={event => changeNameHandler(event)} 
-                        onChange={event => setListName(event.target.value)} 
+                        onKeyUp={event => changeNameHandler(event)}
+                        onChange={event => setListName(event.target.value)}
                         onBlur={() => setEditMode(false)}
                         classNameContainer="max-w-[200px] h-12"
                         className="font-black text-2xl text-center"
                     />
                 )}
-                <MessageCountTitle className="ml-5" count={getListTaskDoneCount(nowActiveList.tasks)}/>
+                <MessageCountTitle className="ml-5" count={getListTaskDoneCount(nowActiveList.tasks)} />
             </div>
-            {
-                isScreenMd && (
-                    <div onClick={() => setIsOpenSidebarMenu(true)}>
-                        <MenuRoundedIcon style={{
-                            width: '32px',
-                            height: '32px',
-                            color: settingsStore.theme === 'light' ? '#313131' : '#fff'
-                        }}/>
-                    </div>
-                )
-            }
+            {isScreenMd && (
+                <div onClick={() => setIsOpenSidebarMenu(true)}>
+                    <MenuRoundedIcon
+                        style={{
+                            width: "32px",
+                            height: "32px",
+                            color: settingsStore.theme === "light" ? "#313131" : "#fff",
+                        }}
+                    />
+                </div>
+            )}
+            {nowActiveList.accessEdit && (
+                <Button
+                    onClick={() => setAlertConfirmationToDeleteList(true)}
+                    className="border-red-500 w-10 h-10"
+                    variant="outline"
+                    icon={<DeleteRounded className="text-red-500" />}
+                ></Button>
+            )}
+            <Alert
+                onYes={{
+                    text: "Yes",
+                    event: () => {
+                        setAlertConfirmationToDeleteList(false)
+                        deleteListHandler()
+                    },
+                }}
+                subtitle={`"${nowActiveList.name}" will be permanently deleted`}
+                title="Are you sure?"
+                isOpen={alertConfirmationToDeleteList}
+                onClose={() => setAlertConfirmationToDeleteList(false)}
+            />
         </div>
-    );
-};
+    )
+}
 
-export default ClipboardListTitle;
+export default ClipboardListTitle
